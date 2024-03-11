60ee96c7009007241f7055ebf01968bcccbc82bf
adderlis <adderlis@yahoo.com>
2024-03-10 20:48:37 -0400

    importaciones no usadas en el mainativity

--- app/src/main/java/com/solidtype/atenas_apk_2/MainActivity.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/MainActivity.kt
@@ -3,14 +3,6 @@ package com.solidtype.atenas_apk_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.solidtype.atenas_apk_2.ui.theme.Atenas_apk_2Theme
 import com.solidtype.atenas_apk_2.users.presentation.register.OutlinedTextFieldExample
 
 
