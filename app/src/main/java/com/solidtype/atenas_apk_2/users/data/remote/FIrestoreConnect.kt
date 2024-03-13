77acbd782cf685fe48e78e0be1f5595ff00fe6c8
adderlis <adderlis@yahoo.com>
2024-03-13 01:05:22 -0400

    Registro y login terminado en la logica

--- app/src/main/java/com/solidtype/atenas_apk_2/users/data/remote/FIrestoreConnect.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/users/data/remote/FIrestoreConnect.kt
@@ -8,6 +8,9 @@ import com.google.android.gms.tasks.TaskCompletionSource
 import com.google.firebase.Firebase
 
 import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class FirestoreConnect {
 
@@ -19,6 +22,32 @@ class FirestoreConnect {
     //y devolvia un valor antes de ejecutarse por completo la operacion
     //por eso devuelvo el task con el boolean en la funcion de iccidInvalited para que espere que haga la tarea y luego que finaliza devuelve la tarea con su estado en false o true
     //y luego verifica si el task fue succesfull en al funcion de NewUser para actualizar la informacion 
+suspend fun newUser2(user:Modelo)= withContext(Dispatchers.IO) {
+              if (validateIccid(user.id_licensia)) {
+                  println("ICCID Valida!! ")
+                  try {
+                      db.collection("usuarios").document(user.id_licensia).update(
+                          "nombre", user.nombre,
+                          "apellido", user.apellido,
+                          "id_licensia", user.id_licensia,
+                          "correo", user.correo,
+                          "clave", user.clave,
+                          "nombre_negocio", user.nombre_negocio,
+                          "direccion_negocio", user.direccion_negocio,
+                          "telefono", user.telefono
+
+                      ).await()
+                      println("Datos guardados correctamente")
+                      return@withContext true
+                  } catch (e: Exception) {
+                      println("No se pudo guardar datos en db ex: $e , <--")
+                      return@withContext false
+                  }
+              } else {
+                  return@withContext false
+              }
+    }
+
 
     fun newUser(user: Modelo) {
         iccidInvalited(user.id_licensia)
@@ -51,6 +80,33 @@ class FirestoreConnect {
                 }
             }
     }
+   private suspend fun validateIccid(iccid:String)= withContext(Dispatchers.IO){
+            try {
+               val query= db.collection("usuarios")
+                val docs= query.get().await()
+                var n =0
+                for (document in docs.documents){
+                    n++
+                    println("EL iccid es : $iccid")
+                    println("los documentos $n = ${document.id}")
+
+                    if(iccid == document.id){
+                        println("Este es el documento encontrado: ${document.id}, <--")
+                        println("este es el iCCID: $iccid")
+                        return@withContext true
+
+                    }
+                }
+                println("No se encontro iguales")
+               return@withContext false
+
+            }catch (e: Exception){
+                println("aqui es si no encuentra ningun documento")
+                return@withContext false
+            }
+
+        }
+
 
     fun iccidInvalited(iCCID: String): Task<Boolean> {
         val task = TaskCompletionSource<Boolean>()
--- app/src/main/java/com/solidtype/atenas_apk_2/users/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/users/data/repository/RepositoryImpl.kt
@@ -1,13 +1,9 @@
 package com.solidtype.atenas_apk_2.users.data.repository
 
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseUser
-import com.solidtype.atenas_apk_2.users.data.remote.FireStore
 import com.solidtype.atenas_apk_2.users.data.remote.FirestoreConnect
 import com.solidtype.atenas_apk_2.users.data.remote.Modelo
 import com.solidtype.atenas_apk_2.users.data.remote.RemoteFirebase
-import com.solidtype.atenas_apk_2.users.domain.model.UserModel
 import com.solidtype.atenas_apk_2.users.domain.repository.UserRepository
 import com.solidtype.atenas_apk_2.users.domain.userCase.ValidateResults
 import kotlinx.coroutines.CoroutineScope
@@ -16,32 +12,34 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class RepositoryImpl (private val auth : RemoteFirebase =RemoteFirebase(),
-                      private val store: FireStore =FireStore()): UserRepository {
+                      private val store: FirestoreConnect =FirestoreConnect()): UserRepository {
 
 
     override suspend fun signUp(
-        email: String, clave: String, name: String, sim: String,
-        apellido: String, nnegocio: String,
-        dnegocio: String, telefono: String
-    ): Boolean {
-        var funciona = false
-        val mod = Modelo(
-            name, apellido, sim, email, clave,
-            nnegocio, dnegocio,
-            telefono
-        )
+                email: String, clave: String, name: String, sim: String,
+                apellido: String, nnegocio: String,
+                dnegocio: String, telefono: String
+            ): Boolean {
 
-        if (auth.signup(email, clave)) {
-            actualizarOInsertarModelo(mod)
-        val mod=Modelo(name,apellido,sim, email,clave,
-            nnegocio,dnegocio,
-            telefono)
+                val mod = Modelo(
+                    name, apellido, email, sim, clave,
+                    nnegocio, dnegocio,
+                    telefono
+                )
+                var estado = false
 
+                if (auth.signup(email, clave)) {
+                    val resultado = store.newUser2(mod)
+                    if (resultado) {
+                        estado=true
 
-            funciona = true
-        }
+                        println("se guardaron datos exitosos")
+                    } else {
 
-        return funciona
+                        println("No se guardaron datos seggun signup")
+                    }
+                }
+                return estado
     }
 
 
@@ -58,20 +56,8 @@ class RepositoryImpl (private val auth : RemoteFirebase =RemoteFirebase(),
 
     private suspend fun actualizarOInsertarModelo(modelo: Modelo) {
         // Utiliza una coroutine para manejar la operación de manera asíncrona
-        CoroutineScope(Dispatchers.IO).launch {
-            val resultado = store.actualizarDocumento(modelo)
 
-            withContext(Dispatchers.Main) {
-
-                if (resultado) {
-
-                    println("Exito desde la corrutina")
-                } else {
-
-                    println("La operación falló")
-                }
             }
         }
-    }
-}
+
 
