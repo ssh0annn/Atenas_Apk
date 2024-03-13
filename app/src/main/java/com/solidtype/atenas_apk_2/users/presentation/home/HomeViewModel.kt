578d1dbecc0ad2cdeeed3dc03a15a101ef12314a
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-03-13 17:05:43 -0400

    HomeScreen antes del pull a origin Main

---
+++ app/src/main/java/com/solidtype/atenas_apk_2/users/presentation/home/HomeViewModel.kt
@@ -0,0 +1,20 @@
+package com.solidtype.atenas_apk_2.users.presentation.home
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+    private val _name = MutableLiveData<String>()
+    val name: MutableLiveData<String> = _name
+
+    private val _logeado = MutableLiveData<Boolean>()
+    val logeado: MutableLiveData<Boolean> = _logeado
+
+    fun setName(name: String) {
+        _name.value = name
+    }
+
+    fun setLogeado(logeado: Boolean) {
+        _logeado.value = logeado
+    }
+}
