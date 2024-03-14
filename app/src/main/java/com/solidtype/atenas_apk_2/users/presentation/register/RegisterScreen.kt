b601227d643eade226211694156d63656112ed90
Angel Matos <ney2331sr@gmail.com>
2024-03-14 19:49:19 -0400

    v7

--- app/src/main/java/com/solidtype/atenas_apk_2/users/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/users/presentation/register/RegisterScreen.kt
@@ -1,9 +1,6 @@
 package com.solidtype.atenas_apk_2.users.presentation.register
 
 import android.content.Context
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,17 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
@@ -31,17 +25,15 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -50,7 +42,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.text.isDigitsOnly
@@ -58,9 +49,6 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-
-
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -129,11 +117,14 @@ fun OutlinedTextFieldExample(context: Context, validarr:login_medenview=login_me
                     modifier = Modifier
                         .padding(start = 220.dp)
                 ){
-                    Text(
-                        text = "Ir a login",
-                        color = Color.Blue,
-                        fontSize = 15.sp
-                    )
+                    TextButton(
+                        onClick = {  },
+                        colors = ButtonDefaults.textButtonColors(
+                            contentColor = Color(0xFFEC407A)
+                        )
+                    ) {
+                        Text("TEXT BUTTON")
+                    }
                 }
             }
         }
