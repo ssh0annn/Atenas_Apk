110f497eaaa5e22aae998e08344c719e0e227a7a
adderlis <adderlis@yahoo.com>
2024-03-15 18:56:47 -0400

    No funciona

--- app/src/main/java/com/solidtype/atenas_apk_2/users/domain/userCase/All_useCases.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/users/domain/userCase/All_useCases.kt
@@ -6,8 +6,6 @@ import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.ExisteUsua
 import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.Registrarse
 import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.SignInUseCase
 import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.SignOutUseCase
-import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.SignUpUseCase
-import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.UsuarioExistente
 import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.VerificaICCIDUseCase
 import com.solidtype.atenas_apk_2.users.domain.userCase.implementados.getCurrentUser
 
