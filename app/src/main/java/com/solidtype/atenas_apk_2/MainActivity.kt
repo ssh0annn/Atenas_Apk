d80ca117de42368ce4ee5d12711ab36b27dd5194
adderlis <adderlis@yahoo.com>
2024-03-27 22:30:04 -0400

    nn

--- app/src/main/java/com/solidtype/atenas_apk_2/MainActivity.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/MainActivity.kt
@@ -6,8 +6,6 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-
 import dagger.hilt.android.AndroidEntryPoint
 
 
