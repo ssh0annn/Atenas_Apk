daac3ce1dd8df281fab5d57ff25a7d4ae9553122
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-04-04 10:39:13 -0400

    Antes del pull de main

--- app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/historial/HistorialViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.solidtype.atenas_apk_2.historial_ventas.presentation.historial
-
-class HistorialViewModel {
-}
--- app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/historial/historialScreen.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/historial/historialScreen.kt
@@ -1,2 +1,98 @@
 package com.solidtype.atenas_apk_2.historial_ventas.presentation.historial
 
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistorialScreen() {
+
+    val ventas by rememberSaveable { mutableStateOf(1000) }
+    val datePickerState: DatePickerState = rememberDatePickerState(initialSelectedDateMillis = 1578096000000)
+
+    Column( //Todo
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(//Contenedor
+            modifier = Modifier
+                .padding(top = 32.dp)
+                .width(950.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Reporte",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold
+            ) //Título
+            Row {//Inpus y Area de Ventas
+                Row(
+                    modifier = Modifier
+                        .weight(3f),
+                ) {//Inputs
+                    DatePicker(state = datePickerState, modifier = Modifier.padding(16.dp))
+                }
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    horizontalAlignment = Alignment.End,
+                ) {//Area de Ventas
+                    Text(
+                        text = "Ventas",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "$ventas RD$",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+            ListItem(headlineContent = {
+                //Aquí el menú
+            })
+        }
+    }
+}
+
+//Preview para Vortex T10M (T10MPROPLUS) Horizontal
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true, widthDp = 1080, heightDp = 560)
+@Composable
+fun HistorialScreenPreview() {
+    HistorialScreen()
+}
