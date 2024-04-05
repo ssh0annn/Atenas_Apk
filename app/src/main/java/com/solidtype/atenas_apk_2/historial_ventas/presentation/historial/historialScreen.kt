772c02f5f8b9643f3543b5b9756269d15e11214e
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-04-04 22:25:34 -0400

    Cogiendo pique con los Datapicker, pero estÃan hechos en su dialogo

--- app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/historial/historialScreen.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/historial/historialScreen.kt
@@ -1,7 +1,11 @@
 package com.solidtype.atenas_apk_2.historial_ventas.presentation.historial
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,10 +13,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
@@ -23,56 +32,81 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.solidtype.atenas_apk_2.historial_ventas.presentation.historial.componets.BotonBlanco
+import com.solidtype.atenas_apk_2.historial_ventas.presentation.historial.componets.DatePickerDialogo
+import com.solidtype.atenas_apk_2.historial_ventas.presentation.historial.componets.SelecionarFecha
+import java.text.SimpleDateFormat
+import java.util.Date
 
 
-
+@SuppressLint("SimpleDateFormat")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistorialScreen() {
 
-    val ventas by rememberSaveable { mutableStateOf(1000) }
-    val datePickerState: DatePickerState = rememberDatePickerState(initialSelectedDateMillis = 1578096000000)
+    var ventas by rememberSaveable { mutableIntStateOf(1000) }
 
-    Column( //Todo
+    val datePickerState1: DatePickerState =
+        rememberDatePickerState(initialSelectedDateMillis = System.currentTimeMillis())
+    var showDatePicker1 by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val datePickerState2: DatePickerState =
+        rememberDatePickerState(initialSelectedDateMillis = System.currentTimeMillis())
+    var showDatePicker2 by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+
+
+    Column(
+        //Todo
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState()),
+            .verticalScroll(rememberScrollState())
+            .background(
+                Color(0xFFD7D7D9)
+            ),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Column(//Contenedor
+        Column(
+            //Contenedor
             modifier = Modifier
                 .padding(top = 32.dp)
                 .width(950.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                text = "Reporte",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
+                text = "Reporte", fontSize = 32.sp, fontWeight = FontWeight.Bold
             ) //Título
             Row {//Inpus y Area de Ventas
                 Row(
-                    modifier = Modifier
-                        .weight(3f),
+                    modifier = Modifier.weight(3f), horizontalArrangement = Arrangement.SpaceBetween
                 ) {//Inputs
-                    DatePicker(state = datePickerState, modifier = Modifier.padding(16.dp))
+                    SelecionarFecha("Fecha Inicial", datePickerState1.selectedDateMillis) {
+                        showDatePicker1 = true
+                    }
+                    SelecionarFecha("Fecha Final", datePickerState2.selectedDateMillis) {
+                        showDatePicker2 = true
+                    }
+                    BotonBlanco(text = "Ventas v") {} //Aquí va un selector
                 }
                 Column(
-                    modifier = Modifier
-                        .weight(1f),
+                    modifier = Modifier.weight(1f),
                     horizontalAlignment = Alignment.End,
                 ) {//Area de Ventas
                     Text(
-                        text = "Ventas",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold
+                        text = "Ventas", fontSize = 24.sp, fontWeight = FontWeight.Bold
                     )
                     Text(
                         text = "$ventas RD$",
@@ -82,11 +116,31 @@ fun HistorialScreen() {
                     )
                 }
             }
-            Spacer(modifier = Modifier.height(32.dp))
+            Spacer(modifier = Modifier.height(16.dp))
             ListItem(headlineContent = {
                 //Aquí el menú
             })
         }
+        DatePickerDialogo(
+            showDatePicker = showDatePicker1,
+            datePickerState = datePickerState1,
+            onDismissRequest = {
+                showDatePicker1 = false
+            },
+            onClick = {
+                showDatePicker1 = false
+            }
+        )
+        DatePickerDialogo(
+            showDatePicker = showDatePicker2,
+            datePickerState = datePickerState2,
+            onDismissRequest = {
+                showDatePicker2 = false
+            },
+            onClick = {
+                showDatePicker2 = false
+            }
+        )
     }
 }
 
