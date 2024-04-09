abbda23bf3c2ab516f09d2e5dd10c833ddddfca5
sh0annn <158389925+ssh0annn@users.noreply.github.com>
2024-04-08 23:00:31 -0400

    arreglando la sincronizacion

---
+++ app/src/main/java/com/solidtype/atenas_apk_2/products/data/remoteProFB/QueryDBlocal.kt
@@ -0,0 +1,4 @@
+package com.solidtype.atenas_apk_2.products.data.remoteProFB
+
+class QueryDBlocal {
+}
---
+++ app/src/main/java/com/solidtype/atenas_apk_2/products/data/remoteProFB/SerializableModelProducts.kt
@@ -0,0 +1,3 @@
+package com.solidtype.atenas_apk_2.products.data.remoteProFB
+
+data class SerializableModelProducts()
