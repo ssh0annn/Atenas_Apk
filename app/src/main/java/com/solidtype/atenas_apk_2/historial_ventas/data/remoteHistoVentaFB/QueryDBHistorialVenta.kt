5916125eaca9dfba2456effbd43ce3ffd10e1d70
sh0annn <158389925+ssh0annn@users.noreply.github.com>
2024-04-10 02:46:45 -0400

    agregue clase query en historial remote

---
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/data/remoteHistoVentaFB/QueryDBHistorialVenta.kt
@@ -0,0 +1,4 @@
+package com.solidtype.atenas_apk_2.historial_ventas.data.remoteHistoVentaFB
+
+class QueryDBHistorialVenta {
+}
---
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/data/remoteHistoVentaFB/mediator/MediatorHistorialVentas.kt
@@ -0,0 +1,17 @@
+package com.solidtype.atenas_apk_2.historial_ventas.data.remoteHistoVentaFB
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.solidtype.atenas_apk_2.products.data.remoteProFB.QuerysFirstore
+import javax.inject.Inject
+
+class MediatorHistorialVentas @Inject constructor(
+    fireStore: FirebaseFirestore,
+    queriesFireStore: QuerysFirstore
+) {
+
+    suspend fun getAlldatabase (){
+
+    }
+
+
+}
