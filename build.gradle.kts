9f1356b9e5dd404f0169c1d021f60e5d2c07eb88
Adderlis <adderlis@yahoo.com>
2024-04-18 23:10:00 -0700

    nada

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
 
 
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.2.0-alpha16" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 
