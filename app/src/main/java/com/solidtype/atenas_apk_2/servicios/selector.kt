f6585b867dd7918679b1d2626ae4a19b28fd8429
MatosRD <you@example.com>
2024-05-31 00:43:32 -0400

    v2.1

--- app/src/main/java/com/solidtype/atenas_apk_2/servicios/selector.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/servicios/selector.kt
@@ -2,15 +2,20 @@ package com.solidtype.atenas_apk_2.servicios
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.SupervisedUserCircle
@@ -19,13 +24,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.solidtype.atenas_apk_2.Authentication.presentation.register.onClick
 import com.solidtype.atenas_apk_2.ui.theme.AzulGris
 import com.solidtype.atenas_apk_2.ui.theme.Blanco
@@ -35,6 +46,7 @@ import com.solidtype.atenas_apk_2.ui.theme.GrisOscuro
 import com.solidtype.atenas_apk_2.ui.theme.PurpleGrey80
 import com.solidtype.atenas_apk_2.ui.theme.Rojo
 
+@OptIn(ExperimentalMultiplatform::class)
 @Composable
 fun selector(){
 
@@ -59,7 +71,7 @@ fun selector(){
 
                 Box(modifier = Modifier.clip(RoundedCornerShape(10.dp))){
                     if (!mostrar.value) {
-                        imprimir()
+                        cuerpo2()
                         altura.value = 400.dp
                     } else {
                         cuerpo()
@@ -124,6 +136,8 @@ fun selector(){
     val mostrar1 = remember { mutableStateOf(false) }
     val altura1 = remember { mutableStateOf(300.dp) }
 
+    var servicio by rememberSaveable { mutableStateOf("") }
+
     //cuerpo del modal
     if (openDialog1.value) {
         Box (modifier = Modifier
@@ -137,8 +151,42 @@ fun selector(){
                         openDialog1.value = false
                     },
                     text = {
-                        Box(modifier = Modifier){
-                            cuerpo3()
+                        Box(modifier = Modifier
+                            .fillMaxWidth()
+                            .verticalScroll(rememberScrollState())){
+                            Column (
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                //titulo
+                                Spacer(modifier = Modifier .padding(top = 15.dp))
+                                Text(
+                                    text = "Nuevo Servicio",
+                                    color = AzulGris,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    fontSize = 35.sp,
+                                )
+
+                                //cuerpo1
+                                Row (
+                                    modifier = Modifier
+                                        .padding(top = 25.dp)
+                                ){
+
+                                    Box (){
+                                        Input(
+                                            label = "Servico",
+                                            valor = servicio,
+                                            derecho = true,
+                                            modifier = Modifier
+                                        ) {
+                                            servicio = it
+                                        }
+                                    }
+                                }
+                            }
                         }
                     },
 
@@ -150,6 +198,7 @@ fun selector(){
 
                             onClick = {
                                 mostrar1.value = true
+
                             }
                         )  {
                             Text("Guardar", color = Blanco)
