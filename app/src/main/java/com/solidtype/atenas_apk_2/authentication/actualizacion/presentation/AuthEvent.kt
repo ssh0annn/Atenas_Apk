c26ed7047a5bedc1dc373490518afe45dfdfe6bb
Adderlis <adderlis@yahoo.com>
2024-06-07 10:44:37 -0400

    Actualizacion del login, ahora se puede guardar el correo en recuerdame

--- app/src/main/java/com/solidtype/atenas_apk_2/authentication/actualizacion/presentation/AuthEvent.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/authentication/actualizacion/presentation/AuthEvent.kt
@@ -3,8 +3,11 @@ package com.solidtype.atenas_apk_2.authentication.actualizacion.presentation
 sealed class AuthEvent {
 
     data class LoginEvent(val email:String, val password:String ):AuthEvent()
+    data class Recuerdame(val email:String?):AuthEvent()
 
     object LogoutEvent:AuthEvent()
 
     object IsAutenticatedEvent:AuthEvent()
+
+
 }
