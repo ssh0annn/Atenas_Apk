a24b8e45d4bb9b924ebb67b0f3b402d1b358bb7d
deadmasacre48 <elcapomax48@gmail.com>
2024-06-26 08:41:33 -0400

    progreso de ventana perfiladmin

--- app/src/main/java/com/solidtype/atenas_apk_2/perfil_administrador/presentation/PerfilAdminScreen.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/perfil_administrador/presentation/PerfilAdminScreen.kt
@@ -1,14 +1,20 @@
 package com.solidtype.atenas_apk_2.perfil_administrador.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
+import com.solidtype.atenas_apk_2.authentication.actualizacion.domain.TipoUser
+import com.solidtype.atenas_apk_2.authentication.actualizacion.presentation.TipoUserSingleton
+import com.solidtype.atenas_apk_2.core.pantallas.Screens
 import com.solidtype.atenas_apk_2.perfil_administrador.presentation.ui.AdminViewModel
 import com.solidtype.atenas_apk_2.util.ui.Components.MenuLateral
 
@@ -16,14 +22,6 @@ import com.solidtype.atenas_apk_2.util.ui.Components.MenuLateral
 fun PerfilAdminScreen(navController: NavController, viewModel: AdminViewModel = hiltViewModel()) {
     val context = LocalContext.current
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-    if(!uiState.perfilAdmin.isEmpty()){
-        print(uiState.perfilAdmin.first())
-        AndroidView(
-            factory = { PefilAdministrador(context, uiState, viewModel) },
-            modifier = Modifier.fillMaxSize()
-        )
-    }else{
 
     /*if (TipoUserSingleton.tipoUser != TipoUser.ADMIN) {
         navController.navigate(Screens.Login.route)
@@ -36,7 +34,13 @@ fun PerfilAdminScreen(navController: NavController, viewModel: AdminViewModel = 
             )
         }
     } else {*/
-
+    if(!uiState.perfilAdmin.isEmpty()){
+        print(uiState.perfilAdmin.first())
+        AndroidView(
+            factory = { PefilAdministrador(context, uiState, viewModel) },
+            modifier = Modifier.fillMaxSize()
+        )
+    }else{
         AndroidView(
             factory = { PefilAdministrador(context, uiState, viewModel) },
             modifier = Modifier.fillMaxSize()
