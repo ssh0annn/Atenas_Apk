5b346f3219e45b088d907030fb31c4116efcc648
deadmasacre48 <elcapomax48@gmail.com>
2024-06-29 13:05:53 -0400

    cositas pasaron, no se cuales son pero pasaron

--- app/src/main/java/com/solidtype/atenas_apk_2/perfil_administrador/domain/modelo/ExtensionesAdmin.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/perfil_administrador/domain/modelo/ExtensionesAdmin.kt
@@ -1,11 +1,6 @@
 package com.solidtype.atenas_apk_2.perfil_administrador.domain.modelo
 
-import androidx.room.PrimaryKey
-import com.solidtype.atenas_apk_2.perfil_administrador.presentation.modelo.PerfilAdmin
 import com.solidtype.atenas_apk_2.perfil_administrador.presentation.modelo.VerInfoAdmin
-
-
-
 
 fun administrador.toVerInfoAdmin(): VerInfoAdmin? {
     return this.estado?.let {
@@ -25,25 +20,6 @@ fun administrador.toVerInfoAdmin(): VerInfoAdmin? {
     )
     }
 }
-
-<<<<<<< HEAD
-fun PerfilAdmin.toAdministrador(): administrador {
-    return administrador(
-        id_administrador = 5000,
-        nombre=this.nombre,
-        apellido=this.apellido,
-        correo=this.correo,
-        telefono=this.telefono,
-        clave= null,
-        direccion_negocio=this.direccion_negocio,
-        nombre_negocio=this.nombre_negocio,
-        licencia=null,
-        fecha_compra=null,
-        fecha_caduca=null,
-        estado=null
-    )
-=======
->>>>>>> 962eaf80d49e37822f832fae950a27a21e5f77cd
 
 
 
