a68a8e3b35bcecff01b21bf5a6a75430b82f93e7
Adderlis <adderlis@yahoo.com>
2024-07-04 01:45:18 -0400

    en proceso

--- app/src/main/java/com/solidtype/AtenasAplication.kt
+++ app/src/main/java/com/solidtype/AtenasAplication.kt
@@ -11,3 +11,24 @@ class AtenasAplication : Application (){
        // Printooth.init(this)
     }
 }
+
+/*
+  override fun onCreate() {
+        super.onCreate()
+
+        // Inicializa y configura WorkManager para sincronización periódica
+        setupPeriodicSync()
+    }
+
+    private fun setupPeriodicSync() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val syncWorkRequest = PeriodicWorkRequestBuilder<SyncWorker>(15, TimeUnit.MINUTES)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(syncWorkRequest)
+    }
+ */
