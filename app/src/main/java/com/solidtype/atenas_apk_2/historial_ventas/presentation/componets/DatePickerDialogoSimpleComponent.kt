51471364540f47d5005a8f2eb877c2db9ff3973c
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-07-25 11:27:03 -0400

    El VM y el uiState ya NO se pasan (intentando dejarlo lo más entendible posible) en los componentes de Historial.

--- app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/componets/DatePickerDialogoSimpleComponent.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/componets/DatePickerDialogoSimpleComponent.kt
@@ -4,23 +4,14 @@ import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import com.solidtype.atenas_apk_2.historial_ventas.presentation.HistorailViewModel
-import com.solidtype.atenas_apk_2.historial_ventas.presentation.HistorialEvent
-import com.solidtype.atenas_apk_2.util.formatearFecha
-import com.solidtype.atenas_apk_2.util.formatoDDBB
-import com.solidtype.atenas_apk_2.util.toLocalDate
 import com.solidtype.atenas_apk_2.util.ui.components.DatePickerDialogo
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerDialogoSimple(
-    identificador: MutableState<String>,
-    selected: MutableState<String>,
     showDatePicker: MutableState<Boolean>,
     datePickerState: DatePickerState,
-    fechaIni: MutableState<String>,
-    viewModel: HistorailViewModel,
-    fechaFin: MutableState<String>
+    onClick: () -> Unit,
 ) {
     DatePickerDialogo(
         showDatePicker = showDatePicker.value,
@@ -28,30 +19,6 @@ fun DatePickerDialogoSimple(
         onDismissRequest = {
             showDatePicker.value = false
         },
-        onClick = {
-            showDatePicker.value = false
-            when (identificador.value) {
-                "FechaIni" -> fechaIni.value = datePickerState.selectedDateMillis.formatearFecha()
-                "FechaFin" -> fechaFin.value = datePickerState.selectedDateMillis.formatearFecha()
-            }
-            when (selected.value) {
-                "Ventas" -> {
-                    viewModel.onEvent(
-                        HistorialEvent.GetVentasFechas(
-                            desde = fechaIni.value.formatoDDBB().toLocalDate(),
-                            hasta = fechaFin.value.formatoDDBB().toLocalDate(),
-                        )
-                    )
-                }
-                "Ticket" -> {
-                    viewModel.onEvent(
-                        HistorialEvent.GetTicketsFechas(
-                            desde = fechaIni.value.formatoDDBB().toLocalDate(),
-                            hasta = fechaFin.value.formatoDDBB().toLocalDate()
-                        )
-                    )
-                }
-            }
-        }
+        onClick = onClick
     )
 }
--- app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/componets/InputsComponent.kt
+++ app/src/main/java/com/solidtype/atenas_apk_2/historial_ventas/presentation/componets/InputsComponent.kt
@@ -1,6 +1,5 @@
 package com.solidtype.atenas_apk_2.historial_ventas.presentation.componets
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -11,9 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.solidtype.atenas_apk_2.historial_ventas.presentation.HistorailViewModel
 import com.solidtype.atenas_apk_2.util.ui.components.SelecionarFecha
-import com.solidtype.atenas_apk_2.util.ui.Pantalla
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,8 +24,9 @@ fun Inputs(
     showDatePicker2: MutableState<Boolean>,
     selected: MutableState<String>,
     ventasTickerTitulo: MutableState<String>,
-    viewModel: HistorailViewModel,
-    modifier: Modifier = Modifier
+    getTodasVentas: () -> Unit,
+    getTodosTickets: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = modifier
@@ -52,6 +50,6 @@ fun Inputs(
             identificador.value = "FechaFin"
         }
         Spacer(modifier = Modifier.width(16.dp))
-        SelectorVentaTicket(selected, ventasTickerTitulo, viewModel, fechaIni, fechaFin)
+        SelectorVentaTicket(selected, ventasTickerTitulo, fechaIni, fechaFin, getTodasVentas, getTodosTickets)
     }
 }
